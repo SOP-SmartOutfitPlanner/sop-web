7429e8cd6ca2bd846c24caf8e892f7c2b7b3bbcc
kwiinie <hau.dtnh@gmail.com>
2025-11-03 00:15:08 +0700

    fix: type error

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,4 @@
 import { ConditionalNavbar } from "@/components/layout/conditional-navbar";
-import GlassCursor from "@/components/ui/glass-cursor";
-import LoadingPreloader from "@/components/ui/preloading";
 
 export default function MainLayout({
   children,
